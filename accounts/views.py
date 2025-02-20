324ce0093cb99b891cb9bf568b73ab4856e64559
Djhonantan Parreira <djhonantanedu@gmail.com>
2025-02-20 14:58:03 -0300

    feat: add home route and template to handle root URL

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
+def home(request):
+    return render(request, 'home.html')
+
 def login(request):
     return render(request, 'login.html')
 
